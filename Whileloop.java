3c05e62a1ea9f7a5c12350ed99d0f70ae790b5cc
Arif <arifadi952@gmail.com>
2021-05-31 23:41:52 +0600

    this is while and do while syntex

---
+++ Whileloop.java
@@ -0,0 +1,15 @@
+
+package javaapplication;
+
+public class Whileloop {
+    public static void main(String[] args)
+    {
+        int i;
+        i=1;
+        while(i<=10)
+        {
+            System.out.println(i+" Garman");
+            i++;
+        }
+    }
+}
---
+++ dowhile.java
@@ -0,0 +1,17 @@
+
+
+package javaapplication;
+
+
+
+public class dowhile {
+    public static void main(String[] args)
+    {
+        int i;
+        i=1;
+        do{
+            System.out.println(i+" Garman");
+            i++;
+        }while(i<=10);
+    }
+}
